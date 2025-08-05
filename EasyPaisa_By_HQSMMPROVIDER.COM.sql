52b4a046c628ff3e2205fb63f7f8582016cc19f2
Your Name <aliabbaszounr1@gmail.com>
2025-08-05 14:45:44 +0500

    Initial commit: Easypaisa Auto Module

---
+++ EasyPaisa_By_HQSMMPROVIDER.COM.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO `payments` (`id`, `type`, `name`, `min`, `max`, `new_users`, `status`, `params`) VALUES
+(16, 'easypaisa', 'Easypaisa', 10, 10000000, 1, 1, '{\"type\":\"easypaisa\",\"name\":\"Easypaisa\",\"min\":\"10\",\"max\":\"10000000\",\"new_users\":\"1\",\"status\":\"1\",\"option\":{\"tnx_fee\":\"0\",\"title\":\"ABCD XYZ\",\"number\":\"0123456789\",\"gmail\":\"techniques77@gmail.com \",\"gmail_password\":\"zkkbjslhqrvawfsu\",\"rate_to_usd\":\"185\"}}');
+
